1765c42f5320c9cf7f7fee7b8256781faa9131b0
James Jahner <jitone1@gmail.com>
2024-11-15 00:31:08 -0600

    add js to zelwk

---
+++ HW/36CLASS/zellwk/public/main.js
@@ -0,0 +1,9 @@
+// main.js
+const update = document.querySelector('#update-button')
+
+update.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+  })
+})
