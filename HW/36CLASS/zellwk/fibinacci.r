2e634d9e2128ea1f951e0c2bed69b4a51abcb1c6
James Jahner <jitone1@gmail.com>
2024-11-14 23:54:19 -0600

    zellwk add mondgodb

---
+++ HW/36CLASS/zellwk/fibinacci.r
@@ -0,0 +1,24 @@
+fibinacci
+function fib(n){
+     if(n<2){return  n}
+    return fib(n-1) + fib(n-2)
+}
+
+
+var fib = function (n){
+
+}
+
+
+
+function fib(3){
+    if(n<2){return  n} 
+
+    return ((fib(n-1)             + fib(n-2))       ) =    
+    return (fib(3-1)              + fib(3-2)        ) =
+    return (fib(2  )              + fib(1  )        ) =
+    return ((fib(2-1) + fib(2-2)) + 1               ) =
+    return ((fib(1)   + fib(0)  ) + 1               ) =
+    return ((1        + 0       ) + 1               ) = 
+    return 2
+}
--- HW/36CLASS/zellwk/server.js
+++ HW/36CLASS/zellwk/server.js
@@ -1,12 +1,39 @@
 console.log('May Node be with you')
 const express = require('express')
 const app = express()
-app.listen(3000, function(){
-    console.log("listening on 3000")
-})
-app.get('/',function(req,res){
-    res.sendFile('/Documents/GitHub/100DEvs/HW/36CLASS/zellwk/index.html')
-})
-app.post('/quotes', (req,res)=>{
-    console.log("Hellooooo!")
-})
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect('mongodb+srv://jamesallenjahner3rd:R69kik0RlJBjcdcV@cluster0.dd2j2.mongodb.net/?retryWrites=true&w=majority&appName=cluster0')
+    .then(client => { 
+        console.log('Connected to Database');
+        const db = client.db('yoda_quotes');
+        const quotesCollection = db.collection('quotes')
+        app.set("view engine",'ejs')
+        app.use(express.urlencoded({ extended: true }))
+        app.get('/',function(req,res){
+            db.collection('quotes')
+            .find()
+            .toArray()
+            .then(results =>{
+                console.log(results)
+            })
+            .catch(error => console.log(error))
+            res.sendFile('/Documents/GitHub/100DEvs/HW/36CLASS/zellwk/index.html')
+        })
+        app.post('/quotes', (req, res) => {
+            quotesCollection
+              .insertOne(req.body)
+              .then(result => {
+                res.redirect('/')
+              })
+              .catch(error => console.error(error))
+          })
+        app.listen(3000, function(){
+            console.log('Listening on 3000');
+        })
+    })
+    .catch(error => console.error(error));
+    
+// Make sure you place this before your CRUD handlers!
+
+
+
