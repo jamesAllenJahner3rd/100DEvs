c47c8afa5f11183951296d852b6271ded41ce257
James Jahner <jitone1@gmail.com>
2024-11-19 02:27:17 -0600

    node-backend-simple-json is now more readable... maybe..

--- HW/36CLASS/class36-materials/node-backend-simple-json/server.js
+++ HW/36CLASS/class36-materials/node-backend-simple-json/server.js
@@ -8,60 +8,46 @@ const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
+  const readAndEnd =(res, err, data)=>{ res.write(data); res.end()};
+  const aReadFile =(requestedPage) =>{
+    fs.readFile(requestedPage, function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
+      readAndEnd(res,err, data);
+    })
+  };
+  let map ={
+    '/'               : () =>aReadFile('index.html'),
+    '/otherpage'      : () =>aReadFile('otherpage.html'),
+    '/otherotherpage' : () =>aReadFile('otherotherpage.html'),
+    '/css/style.css'  : () =>fs.readFile('css/style.css', function(err, data) {readAndEnd(res,err, data)}),
+    '/js/main.js'     : () =>fs.readFile('js/main.js', function(err, data) {
+                                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                                readAndEnd(res,err, data)
+                                })
   }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
+  if (map[page])map[page]()
   else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
+    if(!('student' in params)){
+      return
+    }
+    if(params['student']== 'leon'){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      const objToJson = {
+        name: "leon",
+        currentOccupation: "Baller"
+      }
+      res.end(JSON.stringify(objToJson));
+    }//student = leon
+    else if(params['student'] != 'leon'){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      const objToJson = {
+        name: "unknown",
+        status: "unknown",
+        currentOccupation: "unknown"
+      }
+      res.end(JSON.stringify(objToJson));
+    }//student != leon
+    
   }else{
     figlet('404!!', function(err, data) {
       if (err) {
@@ -69,10 +55,10 @@ const server = http.createServer((req, res) => {
           console.dir(err);
           return;
       }
-      res.write(data);
-      res.end();
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      readAndEnd(res,err, data);
     });
   }
 });
 
-server.listen(8000);
+server.listen(8000, () => { console.log('Server is listening on port 8000'); });
