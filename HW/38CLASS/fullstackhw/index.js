c06397036697ed0e6ec1f26c648605d9ab22bd9d
James Jahner <jitone1@gmail.com>
2024-11-26 15:40:01 -0600

    added generate ID to index.js

--- HW/38CLASS/fullstackhw/index.js
+++ HW/38CLASS/fullstackhw/index.js
@@ -1,9 +1,11 @@
 const http = require('http')
+const express = require("express")
+const app = express()
 let notes=[
     {
         id: "1",
         content: "HTML is easy",
-        important: "true"
+        important: true
     },
     {id: "2",
         content: "Browser can execute only JavaScript",
@@ -11,15 +13,71 @@ let notes=[
     },
     {
       id: "3",
-      conten: "GET and POST are the most important  methods of HTTP protocol",
-      important:true  
+      content: "GET and POST are the most important  methods of HTTP protocol",
+      "important": true
     }
 ]
+app.use(express.json());
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-Type': 'application/json'})
+//     response.end(JSON.stringify(notes))
+// });
+app.post("/api/notes", (request, response)=>{
+    if (!body.content) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }const generateId = () => {
+        const maxId = notes.length > 0
+          ? Math.max(...notes.map(n => Number(n.id)))
+          : 0
+        return String(maxId + 1)
+      }
+      
+      app.post('/api/notes', (request, response) => {
+        const body = request.body
+      
+        if (!body.content) {
+          return response.status(400).json({ 
+            error: 'content missing' 
+          })
+        }
+      
+        const note = {
+          content: body.content,
+          important: Boolean(body.important) || false,
+          id: generateId(),
+        }
+      
+        notes = notes.concat(note)
+      
+        response.json(note)
+      })
+});
+app.get('/',(request, response) => {
+    response.send('<h1> Hello World!</h1>')
+});
+app.get('/api/notes',(request, response)=> {
+    response.json(notes)
+});
+app.get('/api/notes/:id',(request, response) =>{
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json'})
-    response.end(JSON.stringify(notes))
-})
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+
+    if(note){
+        response.json(note)
+    }else{
+        response.status(404).end()
+    };
+});
+app.delete('/api/notes/:id', (request,response) => {
+    const id = request.params.id
+    notes= notes.filter(note => note.id !== id)
+    response.status(204).end()
+});
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+
