be1465dd11c96b4b857143407a27a7b26fa82e55
James Jahner <jitone1@gmail.com>
2024-11-28 16:40:40 -0600

    Created the sever.js for myy Inverntory api

---
+++ HW/38CLASS/endcapapi/server.js
@@ -0,0 +1,8 @@
+const http =require('http')
+const fs = require("fs")    
+const path = require("path")    
+const url = require("url")
+const querystring = require("querystring")
+const express = require("express")
+const app = express()
+app.use(express.static(path.))
